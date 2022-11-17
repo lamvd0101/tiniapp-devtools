c0159fea66c8ac59371d4f5fb69d99773189e091
Liam Vuong <lamvd0101@gmail.com>
2022-11-17 19:34:36 +0700

    Sync

--- packages/tiniapp-cli/src/constants/commands.ts
+++ packages/tiniapp-cli/src/constants/commands.ts
@@ -1,7 +1,37 @@
-export const COMMAND_LIST = {
-  i: 'init',
-  b: 'build',
-  s: 'server',
-  p: 'pack',
-  u: 'upload',
+type Command = {
+  name: string;
+  description: string;
+  argument?: string;
+};
+
+// Commands
+export const INIT_COMMAND: Command = {
+  name: 'init',
+  description: 'Create a Tini App',
+};
+
+export const SERVE_COMMAND: Command = {
+  name: 'serve',
+  description: '',
+};
+
+export const BUILD_COMMAND: Command = {
+  name: 'build',
+  description: 'Build the Tini App',
+};
+
+export const PACK_COMMAND: Command = {
+  name: 'pack',
+  description: '',
+};
+
+export const UPLOAD_COMMAND: Command = {
+  name: 'upload',
+  description: '',
+};
+
+export const VERSION_COMMAND: Command = {
+  name: 'version',
+  description: '',
+  argument: 'v',
 };
