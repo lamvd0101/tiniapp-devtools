197399ca45639406b97e478ce6942a8f9c647798
Liam Vuong <lamvd0101@gmail.com>
2022-11-17 20:06:04 +0700

    Sync

--- packages/tiniapp-compiler/src/bundlers/vite/index.ts
+++ packages/tiniapp-compiler/src/bundlers/vite/index.ts
@@ -3,20 +3,17 @@ import path from 'path';
 import {defineConfig as viteDefineConfig} from 'vite';
 
 export function defineConfig(opts: {name: string; dir: string}) {
-  return viteDefineConfig(({mode}) => {
-    return {
-      mode: mode,
-      root: path.resolve(opts.dir, 'src'),
-      plugins: [nodeResolve({exportConditions: ['node']})],
-      build: {
-        lib: {
-          name: opts.name,
-          fileName: 'index',
-          entry: 'index.ts',
-          formats: ['es'],
-        },
-        outDir: '../dist',
+  return viteDefineConfig({
+    root: path.resolve(opts.dir, 'src'),
+    plugins: [nodeResolve({exportConditions: ['node']})],
+    build: {
+      lib: {
+        name: opts.name,
+        fileName: 'index',
+        entry: 'index.ts',
+        formats: ['es'],
       },
-    };
+      outDir: '../dist',
+    },
   });
 }
